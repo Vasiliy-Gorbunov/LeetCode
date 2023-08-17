efa1bffb5ead62f5ef28b392c4e23e7cb8b637e0
Vasiliy-Gorbunov <79896130110@yandex.ru>
2023-08-17 16:47:06 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,31 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
+    public int romanToInt(String s) {
+        int integer = 0;
+        int prevValue = 0;
+        HashMap<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanValues.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                integer -= currentValue;
+            } else {
+                integer += currentValue;
+                prevValue = currentValue;
             }
         }
-        return null;
+        return integer;
     }
 }
