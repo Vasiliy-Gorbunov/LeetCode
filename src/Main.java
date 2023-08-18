e9f74ddb0b884c65e594a855cc7c9cd6fb1f755a
Vasiliy-Gorbunov <79896130110@yandex.ru>
2023-08-18 12:32:46 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(isPalindrome(123321));
 
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
-        return null;
+        String num = String.valueOf(x);
+        String reverse = new StringBuilder(num).reverse().toString();
+        return num.equals(reverse);
     }
 }
